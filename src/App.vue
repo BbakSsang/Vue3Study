9f456fd7422b10a4f51098b798178152c38d8d36
bbakssang <alexpark216@gmail.com>
2022-06-24 01:31:17 +0900

    3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>TodoList</h1>
+    <TodoForm @todoAdd="todoAddapp" />
+    <TodoList
+      :todolist="todolist"
+      @toggle-todo="toggleTodo"
+      @delete-item="deleteitem"
+    />
+
+    <div v-if="!todolist.length">
+      <b>오늘은 갓백수의 삶을 꿈꾸시나용..?</b>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
+import TodoForm from "./components/TodoForm.vue";
+import TodoList from "./components/TodoList.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+let todolist = ref([]);
+
+var deleteitem = (index) => {
+  // console.log(index)
+  todolist.value.splice(index, 1);
+};
+
+var todoAddapp = (todo) => {
+  todolist.value.push(todo);
+};
+
+var toggleTodo = (index) => {
+  todolist.value[index].isdone = !todolist.value[index].isdone;
+};
+
+// let todoStyle = {
+//   textDecoration: "line-through",
+//   color: "gray",
+// };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.todoStyle {
+  color: red;
+  text-decoration: line-through;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul class="list-group" v-for="(i, index) in todolist" v-bind:key="i.id">
+    <li class="list-group-item d-flex align-items-center mt-2">
+      <div class="form-check flex-grow-1">
+        <input
+          class="CheckInput"
+          :value="todolist.isdone"
+          @change="toggleTodo(index)"
+          type="checkbox"
+        />
+        <label class="checkLabel" :class="{ todoStyle: i.isdone }"
+          >{{ i.sbj }}
+        </label>
+      </div>
+
+      <div>
+        <button class="btn btn-danger btn-sm" @click="deleteitem(index)">
+          삭제
+        </button>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+// import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  todolist: {
+    type: Array,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["toggle-todo", "delete-item"]);
+const toggleTodo = (index) => {
+  emit("toggle-todo", index);
+};
+const deleteitem = (index) => {
+  emit("delete-item", index);
+};
+//props:['todolist']//받아오는 이름 + 위에서 사용하는 이름 props는 return역할을 함께함
+</script>
+
+<style></style>
