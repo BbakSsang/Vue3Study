7f98ecf640eb30af79d87eea6d701b5d770c000b
bbakssang <alexpark216@gmail.com>
2022-06-24 23:32:32 +0900

    6-24

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="container">
     <h1>TodoList</h1>
+
+    <input
+      class="form-control"
+      type="text"
+      v-model="search"
+      placeholder="검색"
+    />
+
+    <hr />
+
     <TodoForm @todoAdd="todoAddapp" />
     <TodoList
-      :todolist="todolist"
+      :todolist="searchTodolist"
       @toggle-todo="toggleTodo"
       @delete-item="deleteitem"
     />
@@ -15,19 +25,59 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import axios from "axios";
 import TodoForm from "./components/TodoForm.vue";
 import TodoList from "./components/TodoList.vue";
 
 let todolist = ref([]);
+let getTodolist = async () => {
+  try {
+    let res = await axios.get("http://localhost:3000/todolist");
+    todolist.value = res.data;
+    //console.log(res.data[0].sbj);
+  } catch (err) {
+    console.log(err);
+  }
+};
+getTodolist();
 
-var deleteitem = (index) => {
-  // console.log(index)
+let search = ref("");
+let searchTodolist = computed(() => {
+  if (search.value) {
+    return todolist.value.filter((tmp) => {
+      return tmp.sbj.includes(search.value);
+    });
+  } else {
+    return todolist.value;
+  }
+});
+
+var deleteitem = async (index) => {
+  let id = todolist.value[index].id;
+  try {
+    let res = await axios.delete("http://localhost:3000/todolist/" + id);
+    console.log(res);
+    // console.log(index)
+  } catch (err) {
+    throw err;
+  }
   todolist.value.splice(index, 1);
 };
 
-var todoAddapp = (todo) => {
-  todolist.value.push(todo);
+//비동기로 만든다
+var todoAddapp = async (todo) => {
+  try {
+    //db 저장
+    let res = await axios.post("http://localhost:3000/todolist", {
+      sbj: todo.sbj,
+      isdone: todo.isdone,
+    });
+
+    todolist.value.push(res.data);
+  } catch (err) {
+    throw err;
+  }
 };
 
 var toggleTodo = (index) => {
